8b4a002460b024ccdd97afbbc243c9a62de2b059
Med Agui <simohamedregragui2@gmail.com>
2023-10-11 14:43:39 +0100

    fixing the services section not showing in the light mode

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ServiceCard = ({order, service, content}) => {
   return (
-    <div className={`flex flex-col flex-1 p-8 rounded-2xl w-[330px] min-w-[260px] ${order === '01' ? `bg-secondary text-white dark:bg-white dark:text-primary` : `bg-white dark:bg-tertiary text-white`}`}>
+    <div className={`flex flex-col flex-1 p-8 rounded-2xl w-[330px] min-w-[260px] ${order === '01' ? `bg-secondary text-white dark:bg-white dark:text-primary` : `bg-white dark:bg-tertiary dark:text-white`}`}>
         <h2 className='self-end self-right text-5xl max-sm:text-3xl'>{order}</h2>
         <h1 className='font-normal text-5xl  mt-10 max-sm:mt-6 max-sm:text-4xl'>{service}</h1>
         <p className='mt-10 max-sm:mt-6'>{content}</p>
