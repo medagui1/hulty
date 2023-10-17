91ad5630701cb948eedeb052eeb270cb96499ebe
Med Agui <simohamedregragui2@gmail.com>
2023-10-17 21:28:27 +0100

    changing the button text size

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({label}) => {
   return (
     <button className="bg-secondary px-6 py-3 rounded-xl dark:bg-white ">
-        <p className="text-white text-[10px] font-bold dark:text-primary">{label}</p>
+        <p className="text-white text-[12px] font-bold dark:text-primary">{label}</p>
     </button>
   )
 }
